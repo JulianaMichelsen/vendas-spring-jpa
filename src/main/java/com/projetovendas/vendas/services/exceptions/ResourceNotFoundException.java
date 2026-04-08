3c6f11d7023636d89b05dd256896f866835a0b99
JulianaMichelsen <juju.michelsen@gmail.com>
2026-04-08 15:02:03 -0300

    Exception handling - delete

--- src/main/java/com/projetovendas/vendas/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/projetovendas/vendas/services/exceptions/ResourceNotFoundException.java
@@ -1,6 +1,10 @@
 package com.projetovendas.vendas.services.exceptions;
 
+import java.io.Serial;
+
 public class ResourceNotFoundException extends RuntimeException{
+    @Serial
+    private static final long serialVersionUID =1L;
 
     public ResourceNotFoundException(Object id){
         super("Resource not  found. Id " + id);
